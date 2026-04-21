db9a2911edb4be5fabb555509eed68ecf13549f5
Ninja3031 <chauhanaadirajsingh@gmail.com>
2026-04-22 00:48:45 +0530

    logout user , middleware , cookie , access refresh Token

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -159,7 +159,37 @@ const logInUser = asyncHandler(async (req , res) => {
 })
 
 const logOutUser = asyncHandler(async(req , res) => {
-    User.findById
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set : {
+                refreshToken : undefined
+            }
+        },
+        {
+            new : true
+        }
+    )
+
+    const options = {
+        httpOnly : true,
+        secure : true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken" , options)
+    .clearCookie("refreshToken" , options)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "User logged out successfully"
+        )
+    )
 })
 
-export {registerUser}
+export {registerUser,
+    logInUser,
+    logOutUser
+}
